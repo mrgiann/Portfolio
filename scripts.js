1003e18b12a0040c582e210f8949fe5c6a1fb90e
Renzo Roggiapane <82038942+mrgiann@users.noreply.github.com>
2023-04-13 20:34:58 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
